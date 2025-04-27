5e46758ccf7182f4148e3472c0197ac2741f6ccd
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2025-04-27 13:45:39 +0900

    add recipes

--- eggdishes-main/src/eggdishes_main/cli.py
+++ eggdishes-main/src/eggdishes_main/cli.py
@@ -1,5 +1,9 @@
 import click
 from eggdishes_core.fresh_egg import FreshEgg
+from eggdishes_boiled.boiled_egg import BoiledEgg
+from eggdishes_poached.poached_egg import PoachedEgg
+from eggdishes_scrambled.scrambled_egg import ScrambledEgg
+from eggdishes_sunnysideup.sunnysideup_egg import SunnysideUpEgg
 
 @click.group()
 def cli():
@@ -9,9 +13,24 @@ def cli():
     "recipe",
     help="Show the recipe for an egg dish."
 )
-def show_recipe():
+@click.argument(
+    "dish",
+    type=click.Choice(["boiled", "poached", "scrambled", "sunnysideup", "fresh"]),
+    default="fresh",
+)
+def show_recipe(dish: str):
     """Show the recipe for a fresh egg dish."""
-    egg_dish = FreshEgg()
+    egg_dish = None
+    if dish == "boiled":
+        egg_dish = BoiledEgg()
+    elif dish == "poached":
+        egg_dish = PoachedEgg()
+    elif dish == "scrambled":
+        egg_dish = ScrambledEgg()
+    elif dish == "sunnysideup":
+        egg_dish = SunnysideUpEgg()
+    else:
+        egg_dish = FreshEgg()
     click.echo(f"Recipe for {egg_dish.name}:")
     click.echo(egg_dish.recipe())
 
